5d703d7978f4db41c5092d6da42fe46c7288995b
Ayo1-spec <jixo154@gmail.com>
2025-03-01 18:23:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'TOHID-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '918930358452' ;  // put your number here
+global.pairingNumber = process.env.BOT_NUMBER || '212716394972' ;  // put your number here
 global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
